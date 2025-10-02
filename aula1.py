f89cef42c465c15c3f81b6d1c609f87c0fe7eca0
QUESIA FERNANDA CORREA <quesia_correa@estudante.sesisenai.org.br>
2025-10-02 16:48:29 -0300

    conteudo sobre pint e variaveis

---
+++ aula1.py
@@ -0,0 +1,11 @@
+#como nosso primeiro código, vamos escapar da maldição
+print("ola mundo!")
+#Abaixo esta o comando print com sep e end padrão
+print ("lógica","de","programação",sep="",end="\n")
+#Abaixo cada concatenação e um enter (\n significa eneter)
+print("vai", "imprmir","em" "baixo?",sep="\n")
+print( " vai", end=" ")
+print ("iprimir", end=" ")
+print ("na", end=" ")
+print ("mesma", end=" ")
+print ("linha", end=" ")
---
+++ aula1_variaveis.py
@@ -0,0 +1,10 @@
+#Podemos definir variaveis com o nome dela, seguido pelo
+#sinal de igual, que signfica "atribuir" e depois pelo valor.
+#nome= " Quesia" <- Crie uma variavel nome que recebe o valor "Quesia"
+nome= "Quesia"
+print("O valor de nome é",nome)
+salario= 1450.00
+dias_trabalhados=22
+print("O funcionario é",nome)
+print("Ele trabalhou", dias_trabalhados,"dias")
+print("O salario é",salario)
